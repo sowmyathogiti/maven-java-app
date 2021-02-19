c0215d6ed8994ba0b98f7e5416cb27d1a40bbb4a
sowmya thogiti <sowmyathogiti27@gmail.com>
2021-02-19 15:11:06 -0500

    add maven archetype

---
+++ src/main/java/edu/nwmissouri/bigdata/thogiti/sowmya/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bigdata.thogiti.sowmya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Big data world!" );
+    }
+}
